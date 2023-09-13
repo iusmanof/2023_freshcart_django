8f4960f19d7ec067769dc85aa21fbad96ae56bc9
iusmanof <iusmanof@gmail.com>
2023-09-13 20:41:55 +0300

    feat: create about page

--- src/core/urls.py
+++ src/core/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'core'
 urlpatterns = [
     path('', views.core_view),
+    path('about/', views.core_about, name="about"),
     path('signin/', views.core_signin, name="signin"),
     path('signup/', views.core_signup, name="signup")
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def core_view(request):
     return render(request, 'core/index.html', {})
 
+
 def core_signin(request):
     return render(request, 'core/signin.html', {})
 
+
 def core_signup(request):
     return render(request, 'core/signup.html', {})
+
+
+def core_about(request):
+    return render(request, 'core/about.html', {})
