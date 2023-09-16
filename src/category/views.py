42b95b17e7524cf8f410843515d8dae51f7364fa
iusmanof <iusmanof@gmail.com>
2023-09-16 10:30:28 +0300

    feat: images added

--- src/category/views.py
+++ src/category/views.py
@@ -12,15 +12,15 @@ def category_items(request, category, sub_category):
             case 'bakery':
                 sub_category_name = models.SubCategory.objects.get(id=1)
                 sub_category_products = models.Product.objects.filter(subcategory=1)
-                print(sub_category_products)
                 context = {'name': sub_category_name, 'list': sub_category_products }
             case 'desserts':
-                context = {'list': ['Apple Jam', 'Banana Split', 'Cheesecake', 'Cherry Cheesecake', 'Chocolate Bar',
-                                    'Chocolate Truffle', 'Ice Cream Cone', 'Jam']}
+                sub_category_name = models.SubCategory.objects.get(id=1)
+                sub_category_products = models.Product.objects.filter(subcategory=2)
+                context = {'name': sub_category_name, 'list': sub_category_products }
             case 'drinks':
-                context = {
-                    'list': ['Coconut Milk', 'Hemp Milk', 'Juice Bottle', 'Oat Milk', 'Orange Soda', 'Soda Water',
-                             'The Toast']}
+                sub_category_name = models.SubCategory.objects.get(id=1)
+                sub_category_products = models.Product.objects.filter(subcategory=3)
+                context = {'name': sub_category_name, 'list': sub_category_products}
             case default:
                 return HttpResponseNotFound("Sub_category not found. CAll us and we create it...")
         return render(request, 'category/index.html', context)
